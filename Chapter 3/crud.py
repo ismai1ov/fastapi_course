8a2ca927768abc1ff59afc9da17cc21c4ff01f96
Vugar Ismayilov <ismailovvugar99@gmail.com>
2025-06-10 17:29:33 +0400

    Simple CRUD application

--- Chapter 3/crud.py
+++ Chapter 3/crud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Body
 
 app = FastAPI()
 
@@ -19,20 +19,28 @@ async def get_message(message_id: int) -> str:
 
 # adding data
 @app.post("/message", status_code=status.HTTP_201_CREATED)
-async def create_message(message:str) -> str:
-    pass 
+async def create_message(message:str = Body()) -> str:
+    current_index = int(max(messages_db, key=int)) + 1
+    messages_db[current_index] = message
+    return "Message created!"
+
 
 # change data
 @app.put("/message/{message_id}")
-async def update_message(message_id:int, message:str) -> str:
-    pass
+async def update_message(message_id:int, message:str = Body()) -> str:
+    messages_db[message_id] = message
+    return "Message updated!"
+
 
 # deleting data by identifier
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: int) -> str:
-    pass
+    messages_db.pop(message_id)
+    return f"Message ID = {message_id} deleted!"
+
 
 # deleting all data
 @app.delete("/")
 async def kill_message_all() -> str:
-    pass
+    messages_db.clear()
+    return "All messages deleted!"
